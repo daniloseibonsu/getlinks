0dd784ce5021f731e0dc3967642b1cadb2454483
daniloseibonsu <daniloseibonsu@gmail.com>
2024-03-04 19:09:26 +0000

    'update3'

--- getlink/getlink/settings.py
+++ getlink/getlink/settings.py
@@ -124,14 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'websitelinks')
+STATIC_URL = 'websitelinks/static/'
 
 STATICFILES_DIRS = [
-
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'websitelinks/static')
 ]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
